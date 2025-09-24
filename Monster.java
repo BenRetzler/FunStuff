a54f0b1d1e07b3ff8f40fd52b7f030868d05f483
BenRetzler <28retzbe@students.lovelandschools.org>
2025-09-24 10:39:07 -0400

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Monster
+{
+  private int health, playerLevel;
+  private Weapon monsterAttack;
+  public Monster()
+  {
+    health = 100;
+    playerLevel = 0;
+    monsterAttack = new Weapon();
+  }
+  public Monster(int h, int lvl)
+  {
+    heatlh = h;
+    playerLevel = lvl;
+    monsterAttack = new Weapon("", 
+  }
+}
